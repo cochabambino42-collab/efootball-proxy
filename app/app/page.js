0db59e721dd0b3e0d6879d94b5e5988a16605e29
cochabambino42-collab <cochabambino42@gmail.com>
2026-01-30 18:18:15 -0400

    Add Home component for efootballhub.net

---
+++ app/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>I.R.D. Proxy para efootballhub.net</h1>
+      <p>Sistema en construcción. Pronto estará el dashboard completo.</p>
+      <p>Desplegado 100% gratis en Vercel.</p>
+    </div>
+  );
+}
