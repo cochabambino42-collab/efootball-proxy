4838f29aef5fa48cb1ea78c48909fa2f4d01c9a9
cochabambino42-collab <cochabambino42@gmail.com>
2026-02-01 01:46:41 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'efootballhub.net',
-      },
-    ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Habilita App Router
+  experimental: {
+    appDir: true,
   },
-}
+  // Para cheerio
+  serverExternalPackages: ['cheerio'],
+  // Asegurar que las API routes funcionen
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('cheerio');
+    }
+    return config;
+  }
+};
+
+module.exports = nextConfig;
